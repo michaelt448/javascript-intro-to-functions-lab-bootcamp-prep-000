0c6182390f22f5a0ff07d6d466742aed93c49d42
michael <michaeltilman77@gmail.com>
2018-06-04 23:29:58 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,6 +4,21 @@ function shout(string){
 function whisper(string){
   return string.toLowerCase()
 }
-fucntion logShout(string){
+function logShout(string){
   return console.log(shout(string))
+}
+function logWhisper(string){
+  return console.log(whisper(string))
+}
+function sayHiToGrandma(string){
+  if(string.toLowerCase.equals(string)){
+  return console.log("I can't hear you")
+  } else if (string.toUpperCase.equals(string)){
+    return console.log("YES INDEED!")
+  }
+    else if (string.equals('I love you, Grandma')){
+      return console.log("I love you, too")
+    }
+  }
+  }
 }
